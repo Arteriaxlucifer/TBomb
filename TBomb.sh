7a16781045a3344324addda3862093a92ab387c8
Sparkz-technology <62829664+Sparkz-technology@users.noreply.github.com>
2020-03-29 16:52:10 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -26,7 +26,7 @@ clear
 echo -e "\e[1;31m"
 figlet TBomb
 echo -e "\e[1;34m Created By sparkz_technology \e[1;32m" 
-toilet -f mono12 -F border Sparkz-technology
+toilet -f mono12 -F border Sparkz
 echo -e "\e[4;34m This Bomber Was Created By Sparkz-technology \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: sparkz-technology@gmail.com \e[0m"
